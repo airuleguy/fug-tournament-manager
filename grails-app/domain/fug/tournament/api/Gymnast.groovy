6f0f9c6ff004333399dbbb2fb4e7dee07c72a909
airuleguy <ale.iruleguy@gmail.com>
2019-08-16 22:39:41 -0300

    Changed order in which data is asked for, when creating a gymnast.

--- grails-app/domain/fug/tournament/api/Gymnast.groovy
+++ grails-app/domain/fug/tournament/api/Gymnast.groovy
@@ -4,25 +4,25 @@ class Gymnast {
 
     static constraints = {
         name size: 0..50, nullable: false
+        nationalIdentityDocumentNumber nullable: false, unique: true
         birthDate nullable: false
         gender nullable: false
-        medicalRecordDueDate nullable: true
         club nullable: false
+        medicalRecordDueDate nullable: true
         level nullable: true
         category nullable: true
-        nationalIdentityDocumentNumber nullable: false
     }
 
     static hasMany = [scores: Score]
 
     String name
+    Long nationalIdentityDocumentNumber
     Gender gender
     Date birthDate
     Date medicalRecordDueDate
     Club club
     Level level
     Category category
-    Long nationalIdentityDocumentNumber
 
     String toString() {
         "${this.name} (${this.nationalIdentityDocumentNumber}) [${this.club}]"
